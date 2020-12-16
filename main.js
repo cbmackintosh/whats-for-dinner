19f8374c888420de7455073fcbdc6ee7acde5f9e
Cameron Mackintosh <cbmackintosh@outlook.com>
2020-12-15 19:33:59 -0700

    add query selectors targeting all home view buttons

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 var sides = ['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad', 'Crispy Potatoes', 'Sweet Potato Tots', 'Coconut Rice', 'Caeser Salad', 'Shrimp Summer Rolls', 'Garlic Butter Mushrooms', 'Hush Puppies'];
 var mains = ['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka', 'Thai Yellow Curry', 'Bibimbap', 'Chicken Parmesean', 'Butternut Squash Soup', 'BBQ Chicken Burgers', 'Ramen', 'Empanadas', 'Chicken Fried Rice', 'Sheet Pan Fajitas', 'Margarita Pizza'];
 var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread', 'Peach Cobbler', 'Cheesecake', 'Funfetti Cake', 'Baklava', 'Flan', 'Macarons', 'Macaroons', 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs'];
+
+var findSideDish = document.querySelector('.side-button');
+var findMainDish = document.querySelector('.mains-button');
+var findDessert = document.querySelector('.desserts-button');
+var findEntireMeal = document.querySelector('.entire-meal-button');
+var letsCook = document.querySelector('.lets-cook');
