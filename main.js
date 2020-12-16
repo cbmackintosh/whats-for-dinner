8c8275da41aadce23d2adaeb12cb5a2d11b8f27e
Cameron Mackintosh <cbmackintosh@outlook.com>
2020-12-15 20:22:56 -0700

    Write function to log random meal based on selected radio button

--- main.js
+++ main.js
@@ -7,4 +7,33 @@ var findSideDish = document.querySelector('.side-button');
 var findMainDish = document.querySelector('.mains-button');
 var findDessert = document.querySelector('.desserts-button');
 var findEntireMeal = document.querySelector('.entire-meal-button');
+
 var letsCook = document.querySelector('.lets-cook');
+var cookpot = document.querySelector('.cookpot')
+var foodLog = document.querySelector('.food-log')
+
+letsCook.addEventListener('click', findRandomDish);
+
+function findRandomDish() {
+  hideCookpot()
+  if (findSideDish.checked === true) {
+    foodLog.innerText = sides[Math.floor(Math.random() * sides.length)];
+  } else if (findMainDish.checked === true) {
+    foodLog.innerText = mains[Math.floor(Math.random() * mains.length)];
+  } else if (findDessert.checked === true) {
+    foodLog.innerText = desserts[Math.floor(Math.random() * desserts.length)];
+  }
+  resetRadioButtons()
+}
+
+// HELPER FUNCTIONS:
+
+function hideCookpot() {
+  cookpot.classList.add('hidden');
+}
+
+function resetRadioButtons() {
+  findSideDish.checked = false;
+  findSideDish.checked = false;
+  findSideDish.checked = false;
+}
